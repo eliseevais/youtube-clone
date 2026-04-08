1dc1909d2141e1815115b3b49a0467c8c6d79df8
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2026-04-08 16:27:42 +0300

    feat LeftMenu

--- src/widgets/BaseLayout/ui/LeftMenu/LeftMenu.tsx
+++ src/widgets/BaseLayout/ui/LeftMenu/LeftMenu.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import s from "./LeftMenu.module.css";
+import Link from "next/link";
 
 export const LeftMenu = () => {
   return (
     <aside className={s.leftMenu}>
       <nav className={s.nav}>
-        <a href="#">Главная</a>
-        <a href="#">Главная</a>
-        <a href="#">Главная</a>
+        <Link href="/editor/addVideo">Добавить видео</Link>
+        <Link href="/profile/123">Профиль</Link>
       </nav>
     </aside>
   );
