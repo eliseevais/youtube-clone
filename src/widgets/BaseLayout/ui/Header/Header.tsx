eb50c91b7f8e4792b06da82c555a8ceee162946d
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2026-04-08 16:28:12 +0300

    feat Header (logo and link)

--- src/widgets/BaseLayout/ui/Header/Header.tsx
+++ src/widgets/BaseLayout/ui/Header/Header.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import s from "./Header.module.css";
+import Image from "next/image";
+import Logo from "./logo.png";
+import Link from "next/link";
 
 export const Header = () => {
-  return <header className={s.header}>Header</header>;
+  return (
+    <header className={s.header}>
+      <Link href="/">
+        <Image width={150} src={Logo} alt={"Logo"} />
+      </Link>
+    </header>
+  );
 };
