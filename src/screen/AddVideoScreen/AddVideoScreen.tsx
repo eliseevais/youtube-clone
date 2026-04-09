59edda433144fbdfb038c8a1eef43479239ba27e
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2026-04-09 15:41:24 +0300

    add React-hook-form, Zod, validation on error

--- src/screen/AddVideoScreen/AddVideoScreen.tsx
+++ src/screen/AddVideoScreen/AddVideoScreen.tsx
@@ -1,42 +1,67 @@
 "use client";
+
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { parseYouTube } from "@/shared/libs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 // https://www.youtube.com/watch?v=mc1Z16DzObQ&t
 
-import { useState } from "react";
-import { parseYouTube } from "../../shared/libs";
+type Inputs = {
+  videoUrl: string;
+};
+
+const schema = z.object({
+  videoUrl: z.string().min(1, { message: "Required video url" }),
+});
 
 export const AddVideoScreen = () => {
   const [videoId, setVideoId] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: Inputs) => {
+    const url = data.videoUrl;
+
+    if (!url) return;
+
+    let finalUrl: URL | null = null;
+
+    try {
+      finalUrl = new URL(url);
+    } catch (error) {
+      console.error("error", error);
+    }
+
+    if (!finalUrl) return;
+
+    const videoId = parseYouTube(finalUrl);
+    if (!videoId) return;
+    setVideoId(videoId);
+  };
+
+  console.log("errors", errors);
+
+  const hasVideoInputUrlError = Boolean(errors.videoUrl?.message);
+
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const anyE = e as any;
-          const url = anyE.target.elements[0].value;
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>
+          <input type={"text"} placeholder={"Insert link on YpuTube"} {...register("videoUrl")} />
 
-          if (!url) return;
-
-          let finalUrl: URL | null = null;
-
-          try {
-            finalUrl = new URL(url);
-          } catch (error) {
-            console.error("error", error);
-          }
-
-          if (!finalUrl) return;
-
-          const data = parseYouTube(finalUrl);
-          if (!data) return;
-
-          console.log("e", data);
-
-          setVideoId(data);
-        }}
-      >
-        <input type={"text"} placeholder={"Insert link on YpuTube"} />
+          {hasVideoInputUrlError && <p style={{ color: "red" }}>{errors.videoUrl?.message}</p>}
+        </label>
         <button>load</button>
       </form>
+
       {videoId && (
         <iframe
           width="560"
