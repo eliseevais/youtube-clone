c559cd9d82106614732712a5bc26be96b648dcc5
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2026-04-14 17:14:27 +0300

    add styles for 404

--- src/screen/NotFoundPage/NotFoundPage.tsx
+++ src/screen/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,26 @@
-import NotFound from "./404.webp";
 import Image from "next/image";
+import s from "./NotFoundPage.module.css";
+import Link from "next/link";
+import React from "react";
 
 export const NotFoundPage = () => {
   return (
-    <div>
-      <Image width={500} src={NotFound} alt="404" />
+    <div className={s.container}>
+      <Image
+        className={s.image}
+        unoptimized
+        width={185}
+        height={174}
+        src={"/monkey.png"}
+        alt="404"
+      />
+
+      <p>Эта страница недоступна.</p>
+      <p>Может, поискать что-нибудь другое?</p>
+
+      <Link href="/" className={s.link}>
+        <Image src={"/youtubeLogo.svg"} alt={"Logo"} width={145} height={30} />
+      </Link>
     </div>
   );
 };
